c6f5a344eee5e9afd8bfd9f4c1e280012781b87a
Michiel Tondeur <michiel.tondeur@gmail.com>
2023-08-07 20:34:31 +0200

    test: Create proper tests for Good Wine
    Better tests for Good Wine longer description

--- TypeScript/test/gilded-tros.spec.ts
+++ TypeScript/test/gilded-tros.spec.ts
@@ -2,32 +2,25 @@ import { Item } from "../src/item";
 import { GildedTros } from "../src/gilded-tros";
 describe("GildedTros", () => {
   const items = [
-    new Item("Ring of Cleansening Code", 10, 20),
-    new Item("Good Wine", 2, 0),
-    new Item("Elixir of the SOLID", 5, 7),
-    new Item("B-DAWG Keychain", 0, 80),
-    new Item("B-DAWG Keychain", -1, 80),
-    new Item("Backstage passes for Re:Factor", 15, 20),
-    new Item("Backstage passes for Re:Factor", 10, 49),
-    new Item("Backstage passes for HAXX", 5, 49),
-    new Item("Duplicate Code", 3, 6),
-    new Item("Long Methods", 3, 6),
-    new Item("Ugly Variable Names", 3, 6),
+    // Good Wine
+    // Gets better when getting older, so plus 1
+    new Item("Good Wine", 2, 3),
+    // Gets even better when expired, so plus 2
+    new Item("Good Wine", 0, 3),
+    // new Item("Ring of Cleansening Code", 10, 20)],
+    // new Item("Good Wine", 2, 0)
+    // new Item("Elixir of the SOLID", 5, 7)
+    // new Item("B-DAWG Keychain", 0, 80)
+    // new Item("B-DAWG Keychain", -1, 80)
+    // new Item("Backstage passes for Re:Factor", 15, 20)
+    // new Item("Backstage passes for Re:Factor", 10, 49)
+    // new Item("Backstage passes for HAXX", 5, 49)
+    // new Item("Duplicate Code", 3, 6)
+    // new Item("Long Methods", 3, 6)
+    // new Item("Ugly Variable Names", 3, 6)
   ];
 
-  const results = [
-    "Ring of Cleansening Code",
-    "Good Wine",
-    "Elixir of the SOLID",
-    "B-DAWG Keychain",
-    "B-DAWG Keychain",
-    "Backstage passes for Re:Factor",
-    "Backstage passes for Re:Factor",
-    "Backstage passes for HAXX",
-    "Duplicate Code",
-    "Long Methods",
-    "Ugly Variable Names",
-  ];
+  const results = [4, 5];
 
   describe("updateQuality", () => {
     describe("WHEN calling updateQuality", () => {
@@ -37,7 +30,7 @@ describe("GildedTros", () => {
         app.updateQuality();
 
         for (let i = 0; i < app.items.length; i++) {
-          expect(app.items[i].name).toEqual(results[i]);
+          expect(app.items[i].quality).toEqual(results[i]);
         }
       });
     });
