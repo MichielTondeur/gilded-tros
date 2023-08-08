ece6f6d94852abaaca911ff16fdb5e015b60db05
Michiel Tondeur <michiel.tondeur@gmail.com>
2023-08-08 20:27:09 +0200

    refactor: Cleaned up some conditions

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -15,24 +15,15 @@ export class GildedTros {
     if (item.quality < 50) {
       item.quality = item.quality + 1;
 
-      if (item.sellIn < 11) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+      if (item.sellIn < 11 && item.quality < 50)
+        item.quality = item.quality + 1;
 
-      if (item.sellIn < 6) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-        }
-      }
+      if (item.sellIn < 6 && item.quality < 50) item.quality = item.quality + 1;
     }
 
     item.sellIn = item.sellIn - 1;
 
-    if (item.sellIn < 0) {
-      item.quality = item.quality - item.quality;
-    }
+    if (item.sellIn < 0) item.quality = item.quality - item.quality;
   }
 
   private updateCommon(item: Item) {
